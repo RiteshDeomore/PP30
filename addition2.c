535676e4c841d35e08b4f0c9e46fc12988690d45
RiteshDeomore <103436130+RiteshDeomore@users.noreply.github.com>
2022-05-21 08:30:09 +0530

    Add files via upload

---
+++ addition2.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+
+int addition(int,int);
+
+int main()
+
+
+{
+
+ int a = 10;
+
+ int b = 11;
+
+ int c = 0;
+
+c= addition (inta,intb); 
+
+ 
+printf("%d\n",c);
+
+return 0;
+ }
+
+
+int addition(intNo1 , intNo2);
+{
+
+ int Ans = 0;
+ addtion = No1 + No2;
+
+
+return 0;
+}
+
+
+
+
+
+
+}
